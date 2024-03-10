58aa26919c6f8e3891fd8e374cc1fe2aeb839bdb
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-10 00:14:50 +0000

    Countries module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { EmployeeModule } from './employee/employee.module';
+import { CountriesModule } from './countries/countries.module';
 
 @Module({
-  imports: [AuthModule, UserModule, EmployeeModule],
+  imports: [AuthModule, UserModule, EmployeeModule, CountriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
