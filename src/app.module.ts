c3124436d720539f8c8bf1c2ba925e52c3df1093
Tiago Sousa <ttiagopintosousa97@gmail.com>
2024-03-25 15:09:02 +0000

    Currency module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { EmployeeModule } from './employee/employee.module';
 import { CountriesModule } from './countries/countries.module';
 import { EmailModule } from './email/email.module';
+import { CurrencyModule } from './currency/currency.module';
 
 @Module({
-  imports: [AuthModule, UserModule, EmployeeModule, CountriesModule, EmailModule],
+  imports: [AuthModule, UserModule, EmployeeModule, CountriesModule, EmailModule, CurrencyModule],
   controllers: [AppController],
   providers: [AppService],
 })
