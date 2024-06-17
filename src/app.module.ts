81f08a8369b36968d9d2bb9a87af3bddbbc78685
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-17 06:53:17 +0100

    Auth user controller, service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountriesModule } from './countries/countries.module';
 import { UserModule } from './user/user.module';
+import { AuthUserModule } from './auth-user/auth-user.module';
 
 @Module({
-  imports: [CountriesModule, UserModule],
+  imports: [CountriesModule, UserModule, AuthUserModule],
   controllers: [AppController],
   providers: [AppService],
 })
