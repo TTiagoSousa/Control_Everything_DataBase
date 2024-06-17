39ae5a4723c3553d9415c2a02e48f736a8a499cd
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-06-17 06:52:29 +0100

    User controller, service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountriesModule } from './countries/countries.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CountriesModule],
+  imports: [CountriesModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
